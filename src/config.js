0e27310dd136eb6e4538808f7556bdfae27f42d2
Hannah Schafer <hannahschafer@Hannahs-MacBook-Pro.local>
2020-06-13 12:34:02 -0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,108 @@
+export const categories = [
+  'police',
+	'education',
+	'seniors',
+	'school_nurses',
+	'street_resurfacing',
+	'homeless_services',
+	'ventilators',
+	'ppe',
+	'community_garden',
+	'free_transportation',
+	'mental_health',
+	'head_start',
+]
+
+export const max = 6000000000;
+
+export const data = {
+	'police': {
+		id: 1,
+		title: 'Police',
+		initial_amount: 5000000000,
+	},
+	'arts_and_culture': {
+		id: 2,
+		title: 'Arts and Culture',
+		per_unit: 0.5,
+		initial_amount: 10000000,
+	},
+	'community_garden': {
+		id: 3,
+		title: 'Community Gardens',
+		per_unit: 5000,
+		initial_amount: 10000000,
+		impact: 'community gardens in your city'
+	},
+	'education': {
+		id: 4,
+		title: 'Education',
+		per_unit: 75757,
+		initial_amount: 10000000,
+		impact: 'new teachers',
+	},
+	'free_transportation': {
+		id: 5,
+		title: 'Free Transportation',
+		per_unit: 13.48,
+		initial_amount: 10000000,
+		impact: 'free subway rides'
+	},
+	'head_start': {
+		id: 6,
+		title: 'Head Start',
+		per_unit: 12063.90,
+		initial_amount: 10000000,
+		impact: 'children in Head Start! Children who participate in Head Start are 12% less likely to live in poverty as adults and 29% less likely to receive public assistance'
+	},
+	'homeless_services': {
+		id: 7,
+		title: 'Homeless Services',
+		per_unit: 46948.36,
+		initial_amount: 10000000,
+		impact: 'homeless family shelter units for a year',
+	},
+	'mental_health': {
+		id: 8,
+		title: 'Mental Health Services',
+		per_unit: 200,
+		initial_amount: 10000000,
+		impact: 'therapy sessions'
+	},
+	'ppe': {
+		id: 9,
+		title: 'PPE',
+		per_unit: 5.75,
+		initial_amount: 10000000,
+		impact: 'N95 masks for hospital staff'
+	}, 
+	'school_nurses': {
+		id: 10,
+		title: 'School Nurses',
+		per_unit: 100000,
+		initial_amount: 10000000,
+		impact: 'school nurses'
+	},
+	'seniors': {
+		id: 11,
+		title: 'Seniors',
+		per_unit: 2.22,
+		initial_amount: 10000000,
+		impact: 'meals delivered to seniors every year',
+	},
+	'street_resurfacing': {
+		id: 12,
+		title: 'Street Resurfacing',
+		per_unit: 149253.73,
+		initial_amount: 10000000,
+		impact: 'miles of road resurfaced'
+	},
+	'ventilators': {
+		id: 13,
+		title: 'Ventilators',
+		per_unit: 25000,
+		initial_amount: 10000000,
+		impact: 'ventilators'
+	},
+}
+
