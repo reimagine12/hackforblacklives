bac7e074c4ed91d11017cb1cbe34648da93bf060
Jesse Harold <jharold@macadmins-MacBook-Pro.local>
2020-06-13 13:42:10 -0400

    add local state, separate color and label

--- src/components/ChartBar/ChartBar.js
+++ src/components/ChartBar/ChartBar.js
@@ -3,10 +3,20 @@ import './ChartBar.css';
 
 function ChartBar(props) {
   const { data } = props;
+  const state = {
+    dragging: false,
+    originalValue: data.budget,
+    updatedValue: data.budget,
+    maxValue: 20000000
+  };
+
+  const getHeight = () => state.updatedValue/state.maxValue * 100 + '%';
 
   return (
-    <div id={`chartBar-${data.id}`} className='chartBar' style={{backgroundColor: data.color}}>
-      {data.label} - ${data.budget.toLocaleString()}
+    <div id={`chartBar-${data.id}`} className='chartBar'>
+      <div className='chartBar-label'>{data.label} - ${state.updatedValue.toLocaleString()}</div>
+      <div className='chartBar-color' style={{backgroundColor: data.color, height: getHeight()}}>
+      </div>
     </div>
   );
 }
