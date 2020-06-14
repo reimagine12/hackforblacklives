a9032be11d2980fdf3e99aeb801df01997f5b47c
Hannah Schafer <hannahschafer@Hannahs-MacBook-Pro.local>
2020-06-13 17:12:44 -0700

    initial

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const categories = [
   'police',
-  'education',
   'seniors',
-  'school_nurses',
-  'street_resurfacing',
+  'health_and_hospitals',
+  'infrastructure',
   'homeless_services',
-  'ventilators',
-  'ppe',
-  'community_garden',
-  'free_transportation',
+  'small_business_services',
   'mental_health',
   'head_start',
+  'veterans_services',
+  'fire_and_ems',
+  'housing',
+  'arts_and_culture',
 ];
 
 export const max = 6000000000;
@@ -19,91 +19,85 @@ export const data = {
   'police': {
     id: 1,
     title: 'Police',
-    initial_amount: 5000000000,
+    initial_amount: 5570000000,
     impact: 'all the things that make people healthier and safer. And you defunded the police! Great job',
   },
   'arts_and_culture': {
     id: 2,
     title: 'Arts and Culture',
-    per_unit: 0.5,
-    initial_amount: 10000000,
-  },
-  'community_garden': {
-    id: 3,
-    title: 'Community Gardens',
-    per_unit: 5000,
-    initial_amount: 10000000,
-    impact: 'community gardens in your city'
-  },
-  'education': {
-    id: 4,
-    title: 'Education',
-    per_unit: 75757,
-    initial_amount: 10000000,
-    impact: 'new teachers',
-  },
-  'free_transportation': {
-    id: 5,
-    title: 'Free Transportation',
-    per_unit: 13.48,
-    initial_amount: 10000000,
-    impact: 'free subway rides'
+    per_unit: 12,
+    initial_amount: 137000000,
+    impact: 'visits children can take in their school groups to the metropolitan museum of art'
   },
   'head_start': {
-    id: 6,
+    id: 3,
     title: 'Head Start',
     per_unit: 12063.90,
-    initial_amount: 10000000,
+    initial_amount: 1949000,
     impact: 'children in Head Start! Children who participate in Head Start are 12% less likely to live in poverty as adults and 29% less likely to receive public assistance'
   },
   'homeless_services': {
-    id: 7,
+    id: 4,
     title: 'Homeless Services',
     per_unit: 46948.36,
-    initial_amount: 10000000,
+    initial_amount: 2070000000,
     impact: 'homeless family shelter units for a year',
   },
   'mental_health': {
-    id: 8,
+    id: 5,
     title: 'Mental Health Services',
-    per_unit: 200,
-    initial_amount: 10000000,
-    impact: 'therapy sessions'
+    per_unit: 150,
+    initial_amount: 578000000,
+    impact: 'counseling sessions'
   },
-  'ppe': {
-    id: 9,
-    title: 'PPE',
+  'health_and_hospitals': {
+    id: 6,
+    title: 'Health and Hospitals',
     per_unit: 5.75,
-    initial_amount: 10000000,
+    initial_amount: 773000000,
     impact: 'N95 masks for hospital staff'
-  }, 
-  'school_nurses': {
-    id: 10,
-    title: 'School Nurses',
-    per_unit: 100000,
-    initial_amount: 10000000,
-    impact: 'school nurses'
   },
   'seniors': {
-    id: 11,
+    id: 7,
     title: 'Seniors',
     per_unit: 2.22,
-    initial_amount: 10000000,
+    initial_amount: 385000000,
     impact: 'meals delivered to seniors every year',
   },
-  'street_resurfacing': {
-    id: 12,
-    title: 'Street Resurfacing',
+  'infrastructure': {
+    id: 8,
+    title: 'Infrastructure',
     per_unit: 149253.73,
-    initial_amount: 10000000,
+    initial_amount: 1100000000,
     impact: 'miles of road resurfaced'
   },
-  'ventilators': {
-    id: 13,
-    title: 'Ventilators',
-    per_unit: 25000,
-    initial_amount: 10000000,
-    impact: 'ventilators'
+  'veterans_services': {
+    id: 9,
+    title: 'Veterans Services',
+    per_unit: null,
+    initial_amount: 1125000000,
+    impact: null,
   },
+  'fire_and_ems': {
+    id: 10,
+    title: 'Fire and EMS',
+    per_unit: 1428571.43,
+    initial_amount: 2090000000,
+    impact: 'fire trucks',
+  },
+  'housing': {
+    id: 11,
+    title: 'Housing',
+    per_unit: 2107,
+    initial_amount: 262000000,
+    impact: '2-bedroom apartments with section 8 housing assistance',
+  },
+  'small_business_services': {
+    id: 12,
+    title: 'Small Business Services',
+    per_unit: 75000,
+    initial_amount: 160800000,
+    impact: 'zero interest loans for small businesses',
+  }
 };
 
