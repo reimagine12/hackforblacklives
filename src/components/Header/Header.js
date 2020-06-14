c2d2d5f5f4941df423a9acbb03018940c21bb012
Jesse Harold <jharold@macadmins-MacBook-Pro.local>
2020-06-14 16:52:30 -0400

    bigger logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import logo from './assets/RE12LOGOPNGSM.png';
+import logo from './assets/RE12LOGOLIGHTBG.png';
 
 const Header = () => (
     <Box m={4}>
-         <img src={logo} alt="Reimagine 12 Logo" width="100" height="100" />
+         <img className='r12-logo' src={logo} alt="Reimagine 12 Logo" />
     </Box>
 )
 
