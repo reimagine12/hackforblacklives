c65f0d25cf5bcfd9515aacb90b9becee9a57a8c7
nerdscarf <tiph@nerdscarf.com>
2020-06-14 16:14:52 -0400

    flipped the type for the bar chart

--- src/components/NonProfits/NonProfits.js
+++ src/components/NonProfits/NonProfits.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import cpr from './assets/logo.jpg';
 
 import { nonProfits } from '../../config.js';
 import './NonProfits.css'
@@ -21,6 +22,9 @@ const NonProfits = () => (
         <Card style={{height: '65%', marginRight: '5px'}}>
           <CardContent>
               {nonProfit.name}
+            
+         <img src={cpr} alt="Communities United for Police Reform" width="200" height="200" />
+   
           </CardContent>
         </Card>
         <Button
